4b3108dbbab357feb8ee57f858b46c297e2a74f8
Getachew Mulat <gech4me@gmail.com>
2020-06-25 08:02:03 +0300

    filtering movies based on genre

--- src/main.js
+++ src/main.js
@@ -22,7 +22,7 @@ new Vue({
     components: {
         'movie-list': {
             template: `<div id="movie-list">
-                            <div v-for="movie in movies" class="movie">{{ movie.title }}</div>
+                            <div v-for="movie in filteredMovies" class="movie">{{ movie.title }}</div>
                         </div>`,
             data() {
                 return {
@@ -33,7 +33,21 @@ new Vue({
                     ],
                 }
             },
-            props: ['genre', 'time']
+            props: ['genre', 'time'],
+            computed: {
+                filteredMovies() {
+                    return this.movies.filter(this.filteredPassesGenreMovies);
+                }
+            },
+            methods: {
+                filteredPassesGenreMovies(movie) {
+                    if(!this.genre.length) {
+                      return true;
+                    }
+
+                    return this.genre.find(genre => movie.genre === genre);
+                }
+            }
         },
         'movie-filter': {
             data() {

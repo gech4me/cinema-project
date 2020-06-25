9cbb380912c557b58f75e331a2a38dd0d7e1cac4
Getachew Mulat <gech4me@gmail.com>
2020-06-25 14:48:35 +0300

    moment-timezone configured

--- src/main.js
+++ src/main.js
@@ -1,19 +1,22 @@
 import Vue from 'vue';
 import './style.scss';
 import axios from 'axios';
-window.axios = require('axios');
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-import genres from './util/genres';
 import MovieList from "./components/MovieList";
 import MovieFilter from "./components/MovieFilter";
+import moment from 'moment-timezone';
+
+window.axios = require('axios');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+moment.tz.setDefault("UTC");
+Object.defineProperty(Vue.prototype,'$moment',{ get() { return this.$root.moment; }});
 
 new Vue({
     el: '#app',
     data: {
         genre: [],
         time: [],
-        movies: []
+        movies: [],
+        moment
     },
     methods: {
       checkFilter(category, title, checked) {

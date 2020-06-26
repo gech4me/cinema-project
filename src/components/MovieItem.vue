6b93b9fe578e6f397e360705edeb51d9369ebd90
Getachew Mulat <gech4me@gmail.com>
2020-06-26 21:32:12 +0300

    filtering sessions based on day

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -9,7 +9,7 @@
                 <span class="movie-rating">{{ movie.Rated }}</span>
             </div>
             <div class="movie-sessions">
-                <div v-for="session in sessions" class="session-time-wrapper">
+                <div v-for="session in filteredSessions(sessions)" class="session-time-wrapper">
                     <div class="session-time">{{ formatSessionTime(session.time) }}</div>
                 </div>
             </div>
@@ -24,6 +24,11 @@
         methods: {
             formatSessionTime(raw) {
                 return this.$moment(raw).format("h:mm A")
+            },
+            filteredSessions(sessions) {
+                return sessions.filter(session => {
+                    return this.$moment(session.time).isSame(this.day,'day');
+                })
             }
         }
     }

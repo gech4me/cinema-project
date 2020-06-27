4eb445fc5679e39782c56f20129e95f71f254d11
Getachew Mulat <gech4me@gmail.com>
2020-06-27 18:47:48 +0300

    displaying detail added

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,12 +1,21 @@
 <template>
-    <div>
-        Hello
+    <div id="detail" v-if="movie">
+        <movie-item :movie="movie.movie" :sessions="[]"></movie-item>
     </div>
 </template>
 
 <script>
+    import MovieItem from "./MovieItem";
     export default {
         name: "Detail",
+        components: {MovieItem},
+        props: [ 'movies' ],
+        computed: {
+            movie() {
+                let movie = this.movies.find(movie => movie.id === this.$route.params.id);
+                return movie ? movie : null;
+            }
+        },
         created() {
             console.log(this.$route.params.id);
         }

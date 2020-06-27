24ef9539f6dfc268fb58942f5dcd2d01e9df4253
Getachew Mulat <gech4me@gmail.com>
2020-06-27 18:32:27 +0300

    redirect and detail page

--- src/util/routes.js
+++ src/util/routes.js
@@ -2,6 +2,7 @@ import Overview from "../components/Overview";
 import Detail from "../components/Detail";
 
 export default [
-    { path: '/', component: Overview },
-    { path: '/movie', component: Detail }
+    { path: '/', component: Overview, name: 'home' },
+    { path: '/movie', component: Detail, name: 'movie' },
+    { path: '*', redirect: { name: 'home' } }
 ];

9d5b83fc9b9bb25961a0aa8e0cfdee9b1b13fb4a
Getachew Mulat <gech4me@gmail.com>
2020-06-28 10:02:38 +0300

    movie details updated

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,8 +1,15 @@
 <template>
     <div id="detail" v-if="movie">
-        <movie-item :movie="movie.movie">
+        <movie-item :movie="movie">
             <div class="movie-details">
-                <p class="movie-genre">{{ movie.movie.Genre }}</p>
+                <p class="movie-genre">{{ movie.Genre }}</p>
+                <p class="movie-plot">{{ movie.Plot }}</p>
+                <table>
+                    <tr><td>Released date: </td><td>{{ movie.Released }}</td></tr>
+                    <tr><td>Running time: </td><td>{{ movie.Runtime }}</td></tr>
+                    <tr><td>Director: </td><td>{{ movie.Director }}</td></tr>
+                    <tr><td>Cast: </td><td>{{ movie.Actors }}</td></tr>
+                </table>
             </div>
         </movie-item>
     </div>
@@ -17,7 +24,7 @@
         computed: {
             movie() {
                 let movie = this.movies.find(movie => movie.id === this.$route.params.id);
-                return movie ? movie : null;
+                return movie ? movie.movie : null;
             }
         },
         created() {
